3f9f860c2b9b645d66a5d1583e6e6c3a258367bc
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2019-03-08 09:46:37 +0100

    Create battery.ino

---
+++ battery/battery.ino
@@ -0,0 +1,17 @@
+#define VBATPIN A7
+void setup() {
+
+Serial.begin(115200);
+
+}
+
+void loop() {
+  float measuredvbat = analogRead(VBATPIN);
+  float vbat = measuredvbat;
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 102.4; // convert to voltage
+  Serial.print("VBat: " ); Serial.println(measuredvbat);
+  Serial.print(vbat);
+
+}
